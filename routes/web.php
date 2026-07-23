70d77f346f2534babd097443971fd2435e7e646d
2KAY-kat <dambwedesigns@gmail.com>
2026-07-23 10:17:25 +0200

    broken health configs

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use App\Http\Controllers\AuditController;
 
+Route::get('/health', fn () => response('ok', 200));
+
 Route::get('/', [AuditController::class, 'create'])->name('audits.create');
 Route::get('/audits/{audit}', [AuditController::class, 'show'])->name('audits.show');
 Route::get('/audits', [AuditController::class, 'index'])->name('audits.index');
 
 Route::post('/audits', [AuditController::class, 'store'])->name('audits.store');
 Route::post('/audits/{audit}/rescan', [AuditController::class, 'rescan'])->name('audits.rescan');
-
-Route::get('/health', fn () => response('ok', 200));
