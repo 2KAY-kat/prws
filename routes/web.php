ce9e412d3c25d0e1ea7a8e6334bb8a756229a0f0
2KAY-kat <dambwedesigns@gmail.com>
2026-08-05 11:16:08 +0200

    chore ... added a rate limit to ther posts request create

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,7 @@ Route::get('/', [AuditController::class, 'create'])->name('audits.create');
 Route::get('/audits/{audit}', [AuditController::class, 'show'])->name('audits.show');
 Route::get('/audits', [AuditController::class, 'index'])->name('audits.index');
 
-Route::post('/audits', [AuditController::class, 'store'])->name('audits.store');
-Route::post('/audits/{audit}/rescan', [AuditController::class, 'rescan'])->name('audits.rescan');
+Route::middleware('throttle:10,1')->group(function () {
+    Route::post('/audits', [AuditController::class, 'store'])->name('audits.store');
+    Route::post('/audits/{audit}/rescan', [AuditController::class, 'rescan'])->name('audits.rescan');
+});
